395387581e04201684a1ebf14755ca765c64c8ba
ELY M. <elymbmx@gmail.com>
2026-09-13 01:04:18 -0500

    test

--- src/common/version.h
+++ src/common/version.h
@@ -10,37 +10,37 @@
 #	define VER_STATUS_FULL "RC"
 #	define VER_STATUS_SHORT "rc"
 #	define VER_STATUS_GREEK "\u0433c"
-#	define VER_REVISION 492
+#	define VER_REVISION 531
 #	define VER_FULL "2.4.4 RC"
 #	define VER_SHORT "2.4rc4"
 #	define VER_SHORT_DOTS "2.4.4"
 #	define VER_SHORT_GREEK "2.4\u0433c4"
-#	define VER_RC_REVISION 2, 4, 4, 492
+#	define VER_RC_REVISION 2, 4, 4, 531
 #	define VER_RC_STATUS 2, 4, 4, 2
 /**** Subversion Information ****/
-#	define VER_REVISION_URL "git@github.com:White-Tiger/T-Clock.git"
-#	define VER_REVISION_DATE "2018-01-28 19:22:27 +0000 (Sun, Jan 28 2018)"
-#	define VER_REVISION_HASH "e89dfdb"
-#	define VER_REVISION_TAG "v2.4.4#492-rc"
+#	define VER_REVISION_URL "git@github.com:ELY3M/T-Clock-Redux.git"
+#	define VER_REVISION_DATE "2018-06-08 08:42:06 +0000 (Fri, Jun 08 2018)"
+#	define VER_REVISION_HASH "2eb7e2a"
+#	define VER_REVISION_TAG "v2.4.4#531-rc"
 /**** Date/Time ****/
-#	define VER_TIMESTAMP 1517167741
-#	define VER_TIME_SEC 1
-#	define VER_TIME_MIN 29
-#	define VER_TIME_HOUR 19
-#	define VER_TIME_DAY 28
-#	define VER_TIME_MONTH 1
-#	define VER_TIME_YEAR 2018
-#	define VER_TIME_WDAY 0
-#	define VER_TIME_YDAY 27
-#	define VER_TIME_WDAY_SHORT "Sun"
-#	define VER_TIME_WDAY_FULL "Sunday"
-#	define VER_TIME_MONTH_SHORT "Jan"
-#	define VER_TIME_MONTH_FULL "January"
-#	define VER_TIME "19:29:01"
-#	define VER_DATE "2018-01-28"
-#	define VER_DATE_LONG "Sun, Jan 28, 2018 19:29:01 UTC"
-#	define VER_DATE_SHORT "2018-01-28 19:29:01 UTC"
-#	define VER_DATE_ISO "2018-01-28T19:29:01Z"
+#	define VER_TIMESTAMP 1744310550
+#	define VER_TIME_SEC 30
+#	define VER_TIME_MIN 42
+#	define VER_TIME_HOUR 18
+#	define VER_TIME_DAY 10
+#	define VER_TIME_MONTH 4
+#	define VER_TIME_YEAR 2025
+#	define VER_TIME_WDAY 4
+#	define VER_TIME_YDAY 99
+#	define VER_TIME_WDAY_SHORT "Thu"
+#	define VER_TIME_WDAY_FULL "Thursday"
+#	define VER_TIME_MONTH_SHORT "Apr"
+#	define VER_TIME_MONTH_FULL "April"
+#	define VER_TIME "18:42:30"
+#	define VER_DATE "2025-04-10"
+#	define VER_DATE_LONG "Thu, Apr 10, 2025 18:42:30 UTC"
+#	define VER_DATE_SHORT "2025-04-10 18:42:30 UTC"
+#	define VER_DATE_ISO "2025-04-10T18:42:30Z"
 /**** Helper 'functions' ****/
 #	define VER_IsReleaseOrHigher() ( VER_STATUS >= 3 )
 #	define VER_IsAlpha() ( VER_STATUS == 0 )
